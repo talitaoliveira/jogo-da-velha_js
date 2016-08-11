8042608fe35e5f1d339f23e72cee5983265bec58
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2016-08-11 09:40:10 -0300

    retirando numeros das casas

--- index.php
+++ index.php
@@ -22,19 +22,19 @@
 		</div>
 		<div class="area-jogo">
 			<div class="linha">
-				<div class="casa" id="casa-1">1</div>
-				<div class="casa" id="casa-2">2</div>
-				<div class="casa" id="casa-3">3</div>
+				<div class="casa" id="casa-1"></div>
+				<div class="casa" id="casa-2"></div>
+				<div class="casa" id="casa-3"></div>
 			</div>
 			<div class="linha">
-				<div class="casa" id="casa-4">4</div>
-				<div class="casa" id="casa-5">5</div>
-				<div class="casa" id="casa-6">6</div>
+				<div class="casa" id="casa-4"></div>
+				<div class="casa" id="casa-5"></div>
+				<div class="casa" id="casa-6"></div>
 			</div>
 			<div class="linha">
-				<div class="casa" id="casa-7">7</div>
-				<div class="casa" id="casa-8">8</div>
-				<div class="casa" id="casa-9">9</div>
+				<div class="casa" id="casa-7"></div>
+				<div class="casa" id="casa-8"></div>
+				<div class="casa" id="casa-9"></div>
 			</div>
 		</div>
 	</div>
