52ce8cbb5517211287901765b5f396a99cb5a456
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2016-08-11 09:34:03 -0300

    comentando o código

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* DEFININDO VARIÁVEIS */
 var casas        = ['casa-1','casa-2','casa-3','casa-4','casa-5','casa-6','casa-7','casa-8','casa-9'];
 var casasUsuario = [];
 var casasPc      = [];
@@ -43,7 +44,7 @@ $(".casa").click(function(){
       alert("O JOGO ACABOU OU NAO FOI INICIADO");
    }
 });
-
+/* FUNÇÃO DA AÇÃO DO USUÁRIO */
 function acaoUsuario(casaClicada){
    var indiceCasaClicada   = casas.indexOf(casaClicada.attr('id'));
    if(indiceCasaClicada    == -1){
@@ -51,70 +52,72 @@ function acaoUsuario(casaClicada){
       return false;
    }
 
-   /* PINTA DE VERMELHO */
+   /* PINTA DE CASA DO USUARIO */
    casaClicada.css('background',corUsuario);
    /* INSERINDO NO ARRAY DE CASAS CLICADAS DO USUÁRIO */
    casasUsuario.push(casas[indiceCasaClicada]);
+   /* RETIRANDO A CASA PINTADA DO ARRAY DE CASAS DISPONIVEIS */
    casas.splice(indiceCasaClicada,1);
    return true;
 }
-
+/* FUNÇÃO DA AÇÃO DO PC */
 function acaoPc(){
+   /* VERIFICANDO QUAIS CASAS ESTAO LIVRES */
    var qtdCasasDisponiveis = casas.length;
+   /* GERANDO CASA ALEATORIA A PARTIR DA QTD DE DISPONOIVEIS */
    var casaAleatoria       = Math.floor(Math.random() * qtdCasasDisponiveis);
+   /* PINTANDO A CASA ALEATORIA ESCOLHIDA*/
    $("#"+casas[casaAleatoria]).css("background",corPC);
+   /* INSERINDO NO ARRAY DE CASAS MARCADAS PELO PC */
    casasPc.push(casas[casaAleatoria]);
+   /* RETIRANDO A CASA PINTADA DO ARRAY DE CASAS DISPONIVEIS */
    casas.splice(casaAleatoria,1);
-   verifica(casasPc,"pc")
+   /* VERIFICAÇÃO SE GANHOU */
+   verifica(casasPc,"pc");
 }
 
+/* FUNÇÃO PARA VERIFICAR SE GANHOU OU NÃO */
 function verifica(arrayCasas,jogador){
+   /* DEFINIDO A MENSAGEM DE ACORDO COM O JOGADOR */
    var msg = "VOCE PERDEU";
    if(jogador == 'usuario'){
       msg = "VOCÊ GANHOU!";
    }
-   if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-2') != -1 && 
-      arrayCasas.indexOf('casa-3') != -1){
+   /* VERIFICAÇÕES DAS CASAS DO JOGADOR PARA VER SE ESTÁ EM ALGUM CASO QUE GANHA */
+   if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-2') != -1 && arrayCasas.indexOf('casa-3') != -1){
       alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-4') != -1 && arrayCasas.indexOf('casa-5') != -1 && 
-   arrayCasas.indexOf('casa-6') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-7') != -1 && arrayCasas.indexOf('casa-8') != -1 && 
-   arrayCasas.indexOf('casa-9') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-4') != -1 && 
-   arrayCasas.indexOf('casa-7') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-2') != -1 && arrayCasas.indexOf('casa-5') != -1 && 
-   arrayCasas.indexOf('casa-8') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-3') != -1 && arrayCasas.indexOf('casa-6') != -1 && 
-   arrayCasas.indexOf('casa-9') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-5') != -1 && 
-   arrayCasas.indexOf('casa-9') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}else if(arrayCasas.indexOf('casa-3') != -1 && arrayCasas.indexOf('casa-5') != -1 && 
-   arrayCasas.indexOf('casa-7') != -1){
-   alert(msg);
-   $("#acabou").val("sim");
-   return true;
-}
-return false;
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-4') != -1 && arrayCasas.indexOf('casa-5') != -1 && arrayCasas.indexOf('casa-6') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-7') != -1 && arrayCasas.indexOf('casa-8') != -1 && arrayCasas.indexOf('casa-9') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-4') != -1 && arrayCasas.indexOf('casa-7') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-2') != -1 && arrayCasas.indexOf('casa-5') != -1 && arrayCasas.indexOf('casa-8') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-3') != -1 && arrayCasas.indexOf('casa-6') != -1 && arrayCasas.indexOf('casa-9') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-1') != -1 && arrayCasas.indexOf('casa-5') != -1 && arrayCasas.indexOf('casa-9') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }else if(arrayCasas.indexOf('casa-3') != -1 && arrayCasas.indexOf('casa-5') != -1 && arrayCasas.indexOf('casa-7') != -1){
+      alert(msg);
+      $("#acabou").val("sim");
+      return true;
+   }
+   return false;
 }
 
 
